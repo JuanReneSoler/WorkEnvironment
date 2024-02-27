cf9b7d7015e17fa456fd67ce497289befbbbc524
JuanReneSoler <juanr.soler_93@hotmail.es>
2024-02-27 08:30:44 -0400

    Update linux-config.sh

--- linux-config.sh
+++ linux-config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo pacman -Syu w3m neovim github-cli cmatrix curl htop iftop bmon neofetch sl tmux asciiquarium ranger fish lazygit less python-pynvim which nodejs npm
-pacman -Syu w3m neovim github-cli cmatrix curl htop iftop bmon neofetch sl tmux asciiquarium ranger fish lazygit less python-pynvim which nodejs npm
+sudo pacman -Syu w3m neovim github-cli cmatrix curl htop iftop bmon neofetch sl tmux asciiquarium ranger fish lazygit less python-pynvim which npm
+pacman -Syu w3m neovim github-cli cmatrix curl htop iftop bmon neofetch sl tmux asciiquarium ranger fish lazygit less python-pynvim which npm
 
 
 # google chrome plugins 
