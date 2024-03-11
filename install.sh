a137e7ecb506645daf59c97a929f24236dda95a2
JuanReneSoler <juanr.soler_93@hotmail.es>
2024-03-10 22:18:30 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,12 +14,14 @@ ln -s -r -f ./keys-map.vim ~/.vim/keys-map.vim
 ln -s -r -f ./config.vim ~/.vim/config.vim
 ln -s -r -f ./coc-config.vim ~/.vim/coc-config.vim
 ln -s -r -f ./coc-settings.json ~/.config/nvim/coc-settings.json
-ln -s -r -f ./.zshrc ~/.zshrc
 ln -s -r -f ./tmux.conf ~/.tmux.conf
 ln -s -r -f ./init.vim ~/.config/nvim/init.vim
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+rm ~/.zshrc
+ln -s -r -f ./.zshrc ~/.zshrc
 
 echo finishied!
