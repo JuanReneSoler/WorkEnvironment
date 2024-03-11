7b1cb4f0a5c835700a1911721ff192aa01ca3ca0
JuanReneSoler <juanr.soler_93@hotmail.es>
2024-03-10 21:18:11 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,6 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 
 mkdir ~/.config
 mkdir ~/.config/nvim
-mkdir ~/.config/fish
 
 ln -s -r -f ./vimrc ~/.vim/vimrc
 ln -s -r -f ./plugins.vim ~/.vim/plugins.vim
@@ -15,7 +14,6 @@ ln -s -r -f ./keys-map.vim ~/.vim/keys-map.vim
 ln -s -r -f ./config.vim ~/.vim/config.vim
 ln -s -r -f ./coc-config.vim ~/.vim/coc-config.vim
 ln -s -r -f ./coc-settings.json ~/.config/nvim/coc-settings.json
-ln -s -r -f ./config.fish ~/.config/fish/config.fish
 ln -s -r -f ./tmux.conf ~/.tmux.conf
 ln -s -r -f ./init.vim ~/.config/nvim/init.vim
 
