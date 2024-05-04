08b9e4d5079f8917d9d4bace15ee29f6d72f4e07
JuanReneSoler <juanr.soler_93@hotmail.es>
2024-05-04 00:13:30 -0400

    quite archivo de fish

--- config.fish
+++
@@ -1,7 +0,0 @@
-if status is-interactive
-and not set -q TMUX
-   exec tmux -2 new-session ranger ~
-end
-set -Ux EDITOR /usr/bin/nvim
-set -gx PROJECT_PATHS ~/workspace
-set -gx PATH $PATH ~/.dotnet/tools
