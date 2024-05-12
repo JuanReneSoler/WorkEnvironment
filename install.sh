75fa5718792cab57b372c84c159b24eb381029a9
Juan René Soler Sirí <juanr.soler_93@hotmail.es>
2024-05-11 23:21:46 -0400

    correcciones en la instalacion

--- install.sh
+++ install.sh
@@ -1,13 +1,36 @@
 #!/bin/bash
 
-./linux-config.sh
+sudo pacman -Syu w3m neovim github-cli cmatrix curl htop iftop bmon neofetch sl tmux asciiquarium ranger zsh lazygit less python-pynvim which npm
+# google chrome plugins 
+# postman
+# easy auto refesh
+# gnome extensions
+# dash-to-dock
+# gpaste
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-mkdir ~/.config
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+if [ ! -d "~/.config" ];
+then
+    mkdir ~/.config
+fi
+
+if [ -d "~/Workspace" ];
+then
+    mkdir ~/Workspace/sqldata
+fi
+
+if [ -f "~/.zshrc" ];
+then
+    rm ~/.zshrc
+fi
+
 mkdir ~/.config/nvim
-mkdir /home/juansoler/Workspace/sqldata
 touch ~/.zshenv
 
 sudo ln -s -r -f ./vimrc ~/.vim/vimrc
@@ -18,11 +41,6 @@ sudo ln -s -r -f ./coc-config.vim ~/.vim/coc-config.vim
 sudo ln -s -r -f ./coc-settings.json ~/.config/nvim/coc-settings.json
 sudo ln -s -r -f ./tmux.conf ~/.tmux.conf
 sudo ln -s -r -f ./init.vim ~/.config/nvim/init.vim
-
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-rm ~/.zshrc
-ln -s -r -f ./.zshrc ~/.zshrc
+sudo ln -s -r -f ./.zshrc ~/.zshrc
 
 echo finishied!
